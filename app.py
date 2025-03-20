e29d78e55b5ef5e407baf44199208ea20b7f1b50
nikolp18 <npeneva08@gmail.com>
2025-03-20 14:43:53 +0200

    login, logout and register

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from flask import request, flash, redirect, url_for, session
+from webforms import LoginForm, UserForm
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -53,6 +56,62 @@ class TimeOffRequest(db.Model):
 
 with app.app_context():
 	db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+	return Users.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		username = request.form['username']
+		user = Users.query.filter_by(username=username).first()
+		if user:
+			if check_password_hash(user.password_hash, form.password.data):
+				login_user(user)
+				flash("Login Succesfull")
+				session['username'] = username
+				return redirect(url_for('dashboard'))
+			else:
+				flash("Wrong Password - Try Again")
+		else:
+			flash("User Not Found - Try Again")
+	return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+	logout_user()
+	flash("You Have Been Logged Out")
+	return redirect(url_for('login'))
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def register():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			hashed_pw = generate_password_hash(form.password_hash.data)
+			user = Users(username=form.username.data, name=form.name.data, email=form.email.data, password_hash=hashed_pw)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.username.data = ''
+		form.email.data = ''
+		form.password_hash.data = ''
+		form.role.data = ''
+
+		flash("User Added Successfully")
+	return render_template("add_user.html", 
+		form=form,
+		name=name)
 
 @app.route('/')
 def home():
